cb4b222b906113eb913ba8076dc2392d9ee195b6
dnggs <dnggs09@gmail.com>
2018-02-07 15:52:06 +0700

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,8 +1,8 @@
-cd ~/xmr-stak/build/bin && ./xmr-stak 
-monero
-bytecoin.uk:5555
-24PZu1n4TaDY9SjfowHANgdJq4vRwBxHbJCDmhDgtzWgJaW4MHAUHKNFr6MNqj3PGR4PGXzCGYQw7UemxRoRxCC97qgbRoT.500
-x
-n
-n
-n
+cd ~/xmr-stak/build/bin && wget https://raw.githubusercontent.com/dnggs/vps/master/config.txt && ./xmr-stak 
+
+
+
+
+
+
+
