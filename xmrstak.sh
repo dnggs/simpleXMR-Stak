b69c5c2aa77f41d9f060d37107108ae308a49203
dnggs <dnggs09@gmail.com>
2018-02-08 00:46:03 +0700

    Update xmrstak.sh

--- xmrstak.sh
+++ xmrstak.sh
@@ -1,15 +1,6 @@
+
+
+
 site=https://raw.githubusercontent.com/dnggs/vps/master/xmrstak.sh
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-git clone https://github.com/fireice-uk/xmr-stak.git
-mkdir xmr-stak/build
-cd xmr-stak/build
-cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF ..
-make install
-cd ~/xmr-stak/build/bin
-wget https://raw.githubusercontent.com/dnggs/vps/master/config.txt
-echo "screen -d -r" >> /root/.bashrc
-screen 
-./xmr-stak
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev && git clone https://github.com/fireice-uk/xmr-stak.git && mkdir xmr-stak/build && cd xmr-stak/build && cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .. && make install && cd ~/xmr-stak/build/bin && wget https://raw.githubusercontent.com/dnggs/vps/master/config.txt && echo "screen -d -r" >> /root/.bashrc && screen ./xmr-stak
 
